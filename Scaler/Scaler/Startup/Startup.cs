19b2c0a48feefa1fec83ccd39de78ac1a8ace129
Ramsey Bland <ramsey.bland@gmail.com>
2020-03-07 12:00:19 -0700

    Got it to display the string

--- Scaler/Scaler/Startup/Startup.cs
+++ Scaler/Scaler/Startup/Startup.cs
@@ -3,6 +3,8 @@ using Autofac.Extras.CommonServiceLocator;
 using CommonServiceLocator;
 using Scaler.Business.Interfaces;
 using Scaler.Business.Services;
+using Scaler.Data.Interfaces;
+using Scaler.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,6 +18,7 @@ namespace Scaler.Startup
             var containerBuilder = new ContainerBuilder();
             RegisterViewModels(ref containerBuilder);
             RegisterServices(ref containerBuilder);
+            RegisterRepositories(ref containerBuilder);
             var container = containerBuilder.Build();
 
             var serviceLocator = new AutofacServiceLocator(container);
@@ -30,6 +33,13 @@ namespace Scaler.Startup
         private static void RegisterServices(ref ContainerBuilder containerBuilder)
         {
             containerBuilder.RegisterType<ChordService>().As<IChordService>().SingleInstance();
+            containerBuilder.RegisterType<NeckService>().As<INeckService>().SingleInstance();
+        }
+
+        private static void RegisterRepositories(ref ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<TuningRepository>().As<ITuningRepository>().SingleInstance();
+            containerBuilder.RegisterType<NotesRepository>().As<INoteRepository>().SingleInstance();
         }
     }
 }
--- Scaler/Scaler/ViewModels/MainViewModel.cs
+++ Scaler/Scaler/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Scaler.Business.Interfaces;
+using Scaler.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,30 +11,50 @@ namespace Scaler
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public MainViewModel(IChordService chordService)
+        private readonly INeckService _neckService;
+        private string notesOfSixthString = string.Empty;
+        public MainViewModel(INeckService neckService)
         {
-            _chordService = chordService;
-            ChordNotesCommand = new Command<string>(GetChordNotes);
+            _neckService = neckService;
+            GetNeckNotesCommand = new Command(GetNoteOfNeck);
         }
 
-        public ICommand ChordNotesCommand { get; }
+        public ICommand GetNeckNotesCommand { get; }
 
-        public string DisplayNotes => $"Notes of Chord = {notesOfChord}";
+        public string DisplaySixthString => $"{notesOfSixthString}";
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        private string notesOfChord = string.Empty;
-        private readonly IChordService _chordService;
 
         private void OnPropertyChanged(string name)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
-        private void GetChordNotes(string chord)
+        private void GetNoteOfNeck()
         {
-            notesOfChord = _chordService.GetNotes(chord);
-            OnPropertyChanged(nameof(DisplayNotes));
+            var neckStrings = _neckService.GetAllNotesOfNeck(Core.Enum.Tuning.EStandard); // Dont hardcode the tuning...
+            foreach (var neckString in neckStrings)
+            {
+                switch (neckString.String)
+                {
+                    case 6:
+                        notesOfSixthString = NotesToString(neckString);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            OnPropertyChanged(nameof(DisplaySixthString));
+        }
+
+        private string NotesToString(NeckString neckString)
+        {
+            var notesAsString = new StringBuilder();
+            foreach (var note in neckString.Notes)
+            {
+                notesAsString.Append($"{note.Note} |");
+            }
+            return notesAsString.ToString();
         }
     }
 }
